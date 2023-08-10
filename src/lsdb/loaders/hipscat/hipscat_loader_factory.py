38f4566c0ff54183769d8fbff8e8a31ec68152e6
Sean McGuire <seanmcgu@andrew.cmu.edu>
2023-08-10 16:20:22 -0400

    add docstring

--- src/lsdb/loaders/hipscat/hipscat_loader_factory.py
+++ src/lsdb/loaders/hipscat/hipscat_loader_factory.py
@@ -14,7 +14,22 @@ loader_class_for_catalog_type = {
 }
 
 
-def get_loader_for_type(catalog_type_to_use: CatalogTypeVar, path: str, config: HipscatLoadingConfig):
+def get_loader_for_type(
+        catalog_type_to_use: CatalogTypeVar,
+        path: str,
+        config: HipscatLoadingConfig
+):
+    """Constructs a CatalogLoader that loads a Dataset of the specified type
+
+    Args:
+        catalog_type_to_use (Type[Dataset]): the type of catalog to be loaded. Uses the actual type
+            as the input, not a string or enum value
+        path (str): the path to load the catalog from
+        config (HipscatLoadingConfig): Additional configuration for loading the catalog
+
+    Returns:
+        An initialized CatalogLoader object with the path and config specified
+    """
     if catalog_type_to_use not in loader_class_for_catalog_type:
         raise ValueError(f"Cannot load catalog type: {str(catalog_type_to_use)}")
     LoaderClass = loader_class_for_catalog_type[catalog_type_to_use]
