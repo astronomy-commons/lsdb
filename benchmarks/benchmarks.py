c9e923ceec3871e42b353f23728dac9eb7aa891f
Sean McGuire <seanmcgu@andrew.cmu.edu>
2023-09-06 15:47:53 -0400

    fix benchmark using tests

--- benchmarks/benchmarks.py
+++ benchmarks/benchmarks.py
@@ -5,16 +5,20 @@ https://asv.readthedocs.io/en/stable/writing_benchmarks.html."""
 import os
 
 import lsdb
-from tests.conftest import test_data_dir, SMALL_SKY_DIR_NAME, SMALL_SKY_XMATCH_NAME
+
+TEST_DIR = os.path.join(os.path.dirname(__file__), '..', 'tests')
+DATA_DIR_NAME = "data"
+SMALL_SKY_DIR_NAME = "small_sky"
+SMALL_SKY_XMATCH_NAME = "small_sky_xmatch"
 
 
 def load_small_sky():
-    path = os.path.join(test_data_dir(), SMALL_SKY_DIR_NAME)
+    path = os.path.join(TEST_DIR, DATA_DIR_NAME, SMALL_SKY_DIR_NAME)
     return lsdb.read_hipscat(path, catalog_type=lsdb.Catalog)
 
 
 def load_small_sky_xmatch():
-    path = os.path.join(test_data_dir(), SMALL_SKY_XMATCH_NAME)
+    path = os.path.join(TEST_DIR, DATA_DIR_NAME, SMALL_SKY_XMATCH_NAME)
     return lsdb.read_hipscat(path, catalog_type=lsdb.Catalog)
 
 
