305e7ed5c7ff401999bceb8b2743790fc577dafd
Samuel Wyatt <swyatt@email.arizona.edu>
2023-09-22 16:05:44 -0700

    adding cloud tests to lsdb

---
+++ cloud_tests/lsdb/catalog/test_catalog.py
@@ -0,0 +1,26 @@
+import dask.dataframe as dd
+import pandas as pd
+from hipscat.pixel_math import HealpixPixel
+
+
+def test_catalog_repr_equals_ddf_repr(small_sky_order1_catalog_cloud):
+    assert repr(small_sky_order1_catalog_cloud) == repr(small_sky_order1_catalog_cloud._ddf)
+
+
+def test_catalog_html_repr_equals_ddf_html_repr(small_sky_order1_catalog_cloud):
+    assert small_sky_order1_catalog_cloud._repr_html_() == small_sky_order1_catalog_cloud._ddf._repr_html_()
+
+
+def test_catalog_compute_equals_ddf_compute(small_sky_order1_catalog_cloud):
+    pd.testing.assert_frame_equal(small_sky_order1_catalog_cloud.compute(), small_sky_order1_catalog_cloud._ddf.compute())
+
+
+def test_get_catalog_partition_gets_correct_partition(small_sky_order1_catalog_cloud):
+    for _, row in small_sky_order1_catalog_cloud.hc_structure.get_pixels().iterrows():
+        hp_order = row["Norder"]
+        hp_pixel = row["Npix"]
+        partition = small_sky_order1_catalog_cloud.get_partition(hp_order, hp_pixel)
+        pixel = HealpixPixel(order=hp_order, pixel=hp_pixel)
+        partition_index = small_sky_order1_catalog_cloud._ddf_pixel_map[pixel]
+        ddf_partition = small_sky_order1_catalog_cloud._ddf.partitions[partition_index]
+        dd.utils.assert_eq(partition, ddf_partition)
