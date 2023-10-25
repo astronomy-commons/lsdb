c745b6d184be8e01d70089d7515809f68c3faf10
Sandro Campos <scampos@andrew.cmu.edu>
2023-10-25 15:22:43 -0400

    Add query and assign methods to catalog

--- tests/lsdb/catalog/test_catalog.py
+++ tests/lsdb/catalog/test_catalog.py
@@ -1,5 +1,7 @@
+import dask.array as da
 import dask.dataframe as dd
 import pandas as pd
+import pytest
 from hipscat.pixel_math import HealpixPixel
 
 
@@ -30,3 +32,50 @@ def test_get_catalog_partition_gets_correct_partition(small_sky_order1_catalog):
         partition_index = small_sky_order1_catalog._ddf_pixel_map[pixel]
         ddf_partition = small_sky_order1_catalog._ddf.partitions[partition_index]
         dd.utils.assert_eq(partition, ddf_partition)
+
+
+def test_query_no_arguments(small_sky_order1_catalog):
+    with pytest.raises(ValueError, match="Provided query expression is not valid"):
+        small_sky_order1_catalog.query()
+
+
+def test_query(small_sky_order1_catalog):
+    result_catalog = small_sky_order1_catalog.query("ra > 300 and dec < -50")
+    expected_ddf = small_sky_order1_catalog._ddf.copy()
+    expected_ddf = expected_ddf[
+        (small_sky_order1_catalog._ddf["ra"] > 300) & (small_sky_order1_catalog._ddf["dec"] < -50)
+    ]
+    pd.testing.assert_frame_equal(result_catalog.compute(), expected_ddf.compute())
+
+
+def test_assign_no_arguments(small_sky_order1_catalog):
+    result_catalog = small_sky_order1_catalog.assign()
+    pd.testing.assert_frame_equal(result_catalog._ddf.compute(), small_sky_order1_catalog.compute())
+
+
+def test_assign_with_callable(small_sky_order1_catalog):
+    kwargs = {"new_column": lambda x: x["ra"] + x["dec"]}
+    result_catalog = small_sky_order1_catalog.assign(**kwargs)
+    expected_ddf = small_sky_order1_catalog._ddf.copy()
+    expected_ddf["new_column"] = expected_ddf["ra"] + expected_ddf["dec"]
+    pd.testing.assert_frame_equal(result_catalog.compute(), expected_ddf.compute())
+
+
+def test_assign_with_series(small_sky_order1_catalog):
+    new_series = small_sky_order1_catalog._ddf["ra_error"].map(lambda x: x**2)
+    kwargs = {"new_column": new_series}
+    result_catalog = small_sky_order1_catalog.assign(**kwargs)
+    expected_ddf = small_sky_order1_catalog._ddf.copy()
+    expected_ddf["new_column"] = new_series
+    pd.testing.assert_frame_equal(result_catalog.compute(), expected_ddf.compute())
+
+
+def test_assign_with_invalid_arguments(small_sky_order1_catalog):
+    with pytest.raises(TypeError, match="Column assignment doesn't support type"):
+        small_sky_order1_catalog.assign(new_column=[1, 2, 3])
+    with pytest.raises(ValueError, match="Array assignment only supports 1-D arrays"):
+        small_sky_order1_catalog.assign(new_column=da.ones((10, 10)))
+    with pytest.raises(ValueError, match="Number of partitions do not match"):
+        chunks = small_sky_order1_catalog._ddf.npartitions + 1
+        array = da.random.random(size=10, chunks=chunks)
+        small_sky_order1_catalog.assign(new_column=array)
