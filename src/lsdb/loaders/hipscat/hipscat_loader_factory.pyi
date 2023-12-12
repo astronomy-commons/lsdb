8ea4e2782c7522a4b8344051bc229f391bb733fe
Sean McGuire <seanmcgu@andrew.cmu.edu>
2023-12-12 13:32:13 -0500

    fix isort

--- src/lsdb/loaders/hipscat/hipscat_loader_factory.pyi
+++ src/lsdb/loaders/hipscat/hipscat_loader_factory.pyi
@@ -4,7 +4,6 @@ from lsdb.catalog.dataset.dataset import Dataset
 from lsdb.loaders.hipscat.abstract_catalog_loader import AbstractCatalogLoader, CatalogTypeVar
 from lsdb.loaders.hipscat.hipscat_loading_config import HipscatLoadingConfig
 
-
 def get_loader_for_type(
     catalog_type_to_use: Type[CatalogTypeVar],
     path: str,
