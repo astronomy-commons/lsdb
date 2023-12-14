f9588dceb0443e04c703c21d0c2c92a61832a5e0
Sean McGuire <seanmcgu@andrew.cmu.edu>
2023-12-14 14:09:16 -0500

    add margin catalog

---
+++ src/lsdb/catalog/margin_catalog.py
@@ -0,0 +1,25 @@
+import dask.dataframe as dd
+import hipscat as hc
+
+from lsdb.catalog.dataset.healpix_dataset import HealpixDataset
+from lsdb.types import DaskDFPixelMap
+
+
+class MarginCatalog(HealpixDataset):
+    """LSDB Catalog DataFrame to perform analysis of sky catalogs and efficient
+    spatial operations.
+
+    Attributes:
+        hc_structure: `hipscat.Catalog` object representing the structure
+                      and metadata of the HiPSCat catalog
+    """
+
+    hc_structure: hc.catalog.MarginCatalog
+
+    def __init__(
+        self,
+        ddf: dd.DataFrame,
+        ddf_pixel_map: DaskDFPixelMap,
+        hc_structure: hc.catalog.MarginCatalog,
+    ):
+        super().__init__(ddf, ddf_pixel_map, hc_structure)
---
+++ src/lsdb/loaders/hipscat/margin_catalog_loader.py
@@ -0,0 +1,25 @@
+import dask.dataframe as dd
+import hipscat as hc
+
+from lsdb.catalog.margin_catalog import MarginCatalog
+from lsdb.loaders.hipscat.abstract_catalog_loader import AbstractCatalogLoader
+
+
+class MarginCatalogLoader(AbstractCatalogLoader[MarginCatalog]):
+    """Loads an HiPSCat AssociationCatalog"""
+
+    def load_catalog(self) -> MarginCatalog:
+        """Load a catalog from the configuration specified when the loader was created
+
+        Returns:
+            Catalog object with data from the source given at loader initialization
+        """
+        hc_catalog = self.load_hipscat_catalog()
+        dask_df, dask_df_pixel_map = self._load_dask_df_and_map(hc_catalog)
+        return MarginCatalog(dask_df, dask_df_pixel_map, hc_catalog)
+
+    def load_hipscat_catalog(self) -> hc.catalog.MarginCatalog:
+        """Load `hipscat` library catalog object with catalog metadata and partition data"""
+        return hc.catalog.MarginCatalog.read_from_hipscat(
+            self.path, storage_options=self.storage_options
+        )
