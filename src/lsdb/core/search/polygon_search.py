8f49271c4caebe55a6c33ca15b61a8889b6230c5
Sandro Campos <scampos@andrew.cmu.edu>
2023-12-21 10:48:01 -0500

    Perform polygon search with sphgeom

--- src/lsdb/core/search/polygon_search.py
+++ src/lsdb/core/search/polygon_search.py
@@ -1,28 +1,45 @@
+from typing import List, Tuple
+
 import dask
 import hipscat as hc
+import numpy as np
 import pandas as pd
-from spherical_geometry.polygon import SingleSphericalPolygon
+from hipscat.pixel_math.polygon_filter import CartesianCoordinates, SkyCoordinates
+from lsst.sphgeom import ConvexPolygon, LonLat, UnitVector3d
 
 
 @dask.delayed
-def polygon_filter(data_frame: pd.DataFrame, polygon: SingleSphericalPolygon, metadata: hc.catalog.Catalog):
+def polygon_filter(data_frame: pd.DataFrame, polygon: ConvexPolygon, metadata: hc.catalog.Catalog):
     """Filters a dataframe to only include points within the specified polygon.
 
     Args:
         data_frame (pd.DataFrame): DataFrame containing points in the sky
-        polygon (SingleSphericalPolygon): spherical polygon of interest, used to filter points
+        polygon (ConvexPolygon): Convex spherical polygon of interest, used to filter points
         metadata (hc.catalog.Catalog): hipscat `Catalog` with catalog_info that matches `dataframe`
 
     Returns:
         A new DataFrame with the rows from `dataframe` filtered to only the pixels inside the polygon.
     """
-    data_frame["_INSIDE_POLYGON"] = [
-        polygon.contains_radec(ra, dec)
-        for ra, dec in zip(
-            data_frame[metadata.catalog_info.ra_column].values,
-            data_frame[metadata.catalog_info.dec_column].values,
-        )
-    ]
+    ra_values = np.radians(data_frame[metadata.catalog_info.ra_column].values)
+    dec_values = np.radians(data_frame[metadata.catalog_info.dec_column].values)
+    data_frame["_INSIDE_POLYGON"] = polygon.contains(ra_values, dec_values)
     data_frame = data_frame.loc[data_frame["_INSIDE_POLYGON"]]
     data_frame = data_frame.drop(columns=["_INSIDE_POLYGON"])
     return data_frame
+
+
+def get_cartesian_polygon(vertices: List[SkyCoordinates]) -> Tuple[ConvexPolygon, List[CartesianCoordinates]]:
+    """Creates the convex polygon to filter pixels with. It transforms the vertices, provided
+    in sky coordinates of ra and dec, to their respective cartesian representation on the unit sphere.
+
+    Arguments:
+        vertices (List[SkyCoordinates]): The pairs of ra and dec coordinates, in degrees
+
+    Returns:
+        A tuple, where the first element is the convex polygon object and the second is the
+        list of cartesian coordinates of its vertices.
+    """
+    vertices_vectors = [UnitVector3d(LonLat.fromDegrees(ra, dec)) for ra, dec in vertices]
+    polygon = ConvexPolygon(vertices_vectors)
+    vertices_xyz = [(vector.x(), vector.y(), vector.z()) for vector in vertices_vectors]
+    return polygon, vertices_xyz
--- tests/lsdb/catalog/test_polygon_search.py
+++ tests/lsdb/catalog/test_polygon_search.py
@@ -1,24 +1,30 @@
+import numpy as np
 import pytest
 from conftest import assert_divisions_are_correct
-from spherical_geometry.polygon import SingleSphericalPolygon
+
+from lsdb.core.search.polygon_search import get_cartesian_polygon
 
 
 def test_polygon_search_filters_correct_points(small_sky_order1_catalog):
-    ra, dec = [300, 300, 272, 272], [-50, -55, -55, -50]
-    polygon = SingleSphericalPolygon.from_lonlat(ra, dec)
-    polygon_search_catalog = small_sky_order1_catalog.polygon_search(polygon)
-    for _, row in polygon_search_catalog.compute().iterrows():
-        ra = row[small_sky_order1_catalog.hc_structure.catalog_info.ra_column]
-        dec = row[small_sky_order1_catalog.hc_structure.catalog_info.dec_column]
-        assert polygon.contains_lonlat(ra, dec)
+    vertices = [(300, -50), (300, -55), (272, -55), (272, -50)]
+    polygon, _ = get_cartesian_polygon(vertices)
+    polygon_search_catalog = small_sky_order1_catalog.polygon_search(vertices)
+    polygon_search_df = polygon_search_catalog.compute()
+    ra_values_radians = np.radians(
+        polygon_search_df[small_sky_order1_catalog.hc_structure.catalog_info.ra_column]
+    )
+    dec_values_radians = np.radians(
+        polygon_search_df[small_sky_order1_catalog.hc_structure.catalog_info.dec_column]
+    )
+    assert all(polygon.contains(ra_values_radians, dec_values_radians))
     assert_divisions_are_correct(polygon_search_catalog)
 
 
 def test_polygon_search_filters_partitions(small_sky_order1_catalog):
-    ra, dec = [300, 300, 272, 272], [-50, -55, -55, -50]
-    polygon = SingleSphericalPolygon.from_lonlat(ra, dec)
-    hc_polygon_search = small_sky_order1_catalog.hc_structure.filter_by_polygon(polygon)
-    polygon_search_catalog = small_sky_order1_catalog.polygon_search(polygon)
+    vertices = [(300, -50), (300, -55), (272, -55), (272, -50)]
+    _, vertices_xyz = get_cartesian_polygon(vertices)
+    hc_polygon_search = small_sky_order1_catalog.hc_structure.filter_by_polygon(vertices_xyz)
+    polygon_search_catalog = small_sky_order1_catalog.polygon_search(vertices)
     assert len(hc_polygon_search.get_healpix_pixels()) == len(polygon_search_catalog.get_healpix_pixels())
     assert len(hc_polygon_search.get_healpix_pixels()) == polygon_search_catalog._ddf.npartitions
     for pixel in hc_polygon_search.get_healpix_pixels():
@@ -26,16 +32,14 @@ def test_polygon_search_filters_partitions(small_sky_order1_catalog):
 
 
 def test_polygon_search_empty(small_sky_order1_catalog):
-    ra, dec = [0, 1, 0], [0, 1, 2]
-    polygon = SingleSphericalPolygon.from_lonlat(ra, dec)
-    polygon_search_catalog = small_sky_order1_catalog.polygon_search(polygon)
+    vertices = [(0, 0), (1, 1), (0, 2)]
+    polygon_search_catalog = small_sky_order1_catalog.polygon_search(vertices)
     assert len(polygon_search_catalog.get_healpix_pixels()) == 0
     assert len(polygon_search_catalog.hc_structure.pixel_tree) == 1
 
 
 def test_polygon_search_invalid_shape(small_sky_order1_catalog):
-    # Polygon is not convex, so the shape is invalid
-    ra, dec = [0, 1, 1, 0], [1, 0, 1, 0]
-    polygon = SingleSphericalPolygon.from_lonlat(ra, dec)
+    """The polygon is not convex, so the shape is invalid"""
+    vertices = [(0, 1), (1, 0), (1, 1), (0, 0)]
     with pytest.raises(RuntimeError):
-        small_sky_order1_catalog.polygon_search(polygon)
+        small_sky_order1_catalog.polygon_search(vertices)
