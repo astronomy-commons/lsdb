474acfe22cfca711520d5a447ca8b3feef036116
Sandro Campos <scampos@andrew.cmu.edu>
2023-12-21 11:22:28 -0500

    Update polygon search benchmark

--- benchmarks/benchmarks.py
+++ benchmarks/benchmarks.py
@@ -3,11 +3,7 @@
 For more information on writing benchmarks:
 https://asv.readthedocs.io/en/stable/writing_benchmarks.html."""
 import os
-
-from spherical_geometry.polygon import SingleSphericalPolygon
-
 import lsdb
-from benchmarks.utils import upsample_array
 
 TEST_DIR = os.path.join(os.path.dirname(__file__), "..", "tests")
 DATA_DIR_NAME = "data"
@@ -39,15 +35,7 @@ def time_kdtree_crossmatch():
 
 
 def time_polygon_search():
-    """Time polygonal search using spherical-geometry"""
-    small_sky_order1 = load_small_sky_order1().compute()
-    # Define search polygon
-    polygon_ra, polygon_dec = [300, 300, 272, 272], [-50, -55, -55, -50]
-    polygon = SingleSphericalPolygon.from_lonlat(polygon_ra, polygon_dec)
-    # Upsample test catalog to 10,000 points
-    num_points = 10_000
-    catalog_ra = upsample_array(small_sky_order1["ra"].values, num_points)
-    catalog_dec = upsample_array(small_sky_order1["dec"].values, num_points)
-    # Perform the check for each point on the catalog
-    for ra, dec in zip(catalog_ra, catalog_dec):
-        polygon.contains_lonlat(ra, dec)
+    """Time polygonal search using sphgeom"""
+    small_sky_order1 = load_small_sky_order1()
+    vertices = [(300, -50), (300, -55), (272, -55), (272, -50)]
+    small_sky_order1.polygon_search(vertices)
