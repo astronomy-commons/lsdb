0e1d8415063a252f599e5988841f12eccbef612d
Sean McGuire <seanmcgu@andrew.cmu.edu>
2024-02-01 12:00:10 -0500

    remove duplicated docstring

--- src/lsdb/loaders/hipscat/margin_catalog_loader.py
+++ src/lsdb/loaders/hipscat/margin_catalog_loader.py
@@ -8,11 +8,6 @@ class MarginCatalogLoader(AbstractCatalogLoader[MarginCatalog]):
     """Loads an HiPSCat MarginCatalog"""
 
     def load_catalog(self) -> MarginCatalog:
-        """Load a catalog from the configuration specified when the loader was created
-
-        Returns:
-            Catalog object with data from the source given at loader initialization
-        """
         hc_catalog = self.load_hipscat_catalog()
         dask_df, dask_df_pixel_map = self._load_dask_df_and_map(hc_catalog)
         return MarginCatalog(dask_df, dask_df_pixel_map, hc_catalog)
