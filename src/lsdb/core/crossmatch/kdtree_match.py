45d46f7423a48339bcc7ba98a2d372ba0a392a4a
Sandro Campos <scampos@andrew.cmu.edu>
2024-04-01 17:38:43 -0400

    Add non-matches to knn crossmatch with 1 neighbor

--- src/lsdb/core/crossmatch/kdtree_match.py
+++ src/lsdb/core/crossmatch/kdtree_match.py
@@ -36,6 +36,7 @@ class KdTreeCrossmatch(AbstractCrossmatchAlgorithm):
         self,
         n_neighbors: int = 1,
         radius_arcsec: float = 1,
+        keep_non_matches: bool = False,
         **kwargs,
     ) -> pd.DataFrame:
         """Perform a cross-match between the data from two HEALPix pixels
@@ -46,6 +47,7 @@ class KdTreeCrossmatch(AbstractCrossmatchAlgorithm):
         Args:
             n_neighbors (int): The number of neighbors to find within each point
             radius_arcsec (float): The threshold distance in arcseconds beyond which neighbors are not added
+            keep_non_matches (bool): If True, keeps points from the left with no matches. Defaults to False.
 
         Returns:
             A DataFrame from the left and right tables merged with one row for each pair of
@@ -59,7 +61,7 @@ class KdTreeCrossmatch(AbstractCrossmatchAlgorithm):
 
         # get matching indices for cross-matched rows
         chord_distances, left_idx, right_idx = self._find_crossmatch_indices(
-            n_neighbors=n_neighbors, max_distance=d_chord
+            n_neighbors=n_neighbors, max_distance=d_chord, keep_non_matches=keep_non_matches
         )
         arc_distances = np.degrees(2.0 * np.arcsin(0.5 * chord_distances)) * 3600
 
@@ -68,24 +70,48 @@ class KdTreeCrossmatch(AbstractCrossmatchAlgorithm):
         self._rename_columns_with_suffix(self.right, self.suffixes[1])
 
         # concat dataframes together
+        arc_distances = arc_distances[~np.isnan(arc_distances)]
+        extra_columns = pd.DataFrame({"_dist_arcsec": pd.Series(arc_distances)})
+
+        return self._concat_partial_dataframes(left_idx, right_idx, extra_columns, keep_non_matches)
+
+    def _concat_partial_dataframes(
+        self, left_idx, right_idx, extra_columns, keep_non_matches
+    ) -> pd.DataFrame:
         self.left.index.name = HIPSCAT_ID_COLUMN
         left_join_part = self.left.iloc[left_idx].reset_index()
-        right_join_part = self.right.iloc[right_idx].reset_index(drop=True)
-        out = pd.concat(
-            [
-                left_join_part,
-                right_join_part,
-            ],
-            axis=1,
-        )
+
+        # For the right part we need to decide if we keep the non-matches.
+        # If we do, first we get the rows for the valid matches and set their
+        # crossmatch distance column. Then we fill out the remaining rows for
+        # the non-matches with empty (nan) data.
+
+        # First, get the points from the right that are matches. They are matches
+        # if the index to the right partition is smaller than the length of the
+        # partition and if it is greater than -1.
+        match_cond = np.logical_and(right_idx < len(self.right), right_idx > -1)
+        match_indices = right_idx[np.where(match_cond)]
+        right_join_part = self.right.iloc[match_indices].reset_index(drop=True)
+
+        # For the valid rows, add the extra distance column.
+        self._append_extra_columns(right_join_part, extra_columns)
+
+        if keep_non_matches:
+            # Create an empty Dataframe for all the non-matches
+            num_non_matches = len(right_idx) - len(match_indices)
+            right_non_matches = np.full(fill_value=np.nan, shape=(num_non_matches, right_join_part.shape[1]))
+            non_matches_df = pd.DataFrame(right_non_matches, columns=right_join_part.columns)
+            # Concatenate the non-matches with the matches
+            right_join_part = pd.concat([right_join_part, non_matches_df], ignore_index=True)
+
+        # Finally, concatenate the left and right parts, and set the index
+        out = pd.concat([left_join_part, right_join_part], axis=1)
         out.set_index(HIPSCAT_ID_COLUMN, inplace=True)
-        extra_columns = pd.DataFrame({"_dist_arcsec": pd.Series(arc_distances, index=out.index)})
-        self._append_extra_columns(out, extra_columns)
 
         return out
 
     def _find_crossmatch_indices(
-        self, n_neighbors: int, max_distance: float
+        self, n_neighbors: int, max_distance: float, keep_non_matches: bool
     ) -> Tuple[npt.NDArray[np.float64], npt.NDArray[np.int64], npt.NDArray[np.int64]]:
         # calculate the cartesian coordinates of the points
         left_xyz = _lon_lat_to_xyz(
@@ -121,6 +147,11 @@ class KdTreeCrossmatch(AbstractCrossmatchAlgorithm):
 
         # Infinite distance means no match
         match_mask = np.isfinite(distances)
+
+        if keep_non_matches:
+            neighbors_match_mask = np.ones(match_mask.shape[0], dtype=bool)
+            match_mask = match_mask | neighbors_match_mask
+
         return distances[match_mask], left_index[match_mask], right_index[match_mask]
 
 
