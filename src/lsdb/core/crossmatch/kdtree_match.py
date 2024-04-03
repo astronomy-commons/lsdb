609bf42e5eff7b5c58d751080ff9673998dfb584
Sandro Campos <scampos@andrew.cmu.edu>
2024-04-03 17:34:32 -0400

    Remove leading too close matches from mask

--- src/lsdb/core/crossmatch/kdtree_match.py
+++ src/lsdb/core/crossmatch/kdtree_match.py
@@ -132,7 +132,7 @@ class KdTreeCrossmatch(AbstractCrossmatchAlgorithm):
 
         # We need make the shape the same as for right_index
         if n_neighbors > 1 or min_distance > 0:
-            left_index = np.stack([left_index] * right_index.shape[1], axis=1)
+            left_index = np.stack([left_index] * n_neighbors, axis=1)
 
         # Infinite distance means no match
         match_mask = np.isfinite(distances)
@@ -171,6 +171,7 @@ def _get_chord_distance(radius_arcsec: float) -> float:
     return 2.0 * math.sin(math.radians(0.5 * radius_degrees))
 
 
+# pylint: disable=too-many-locals
 def _query_min_max_neighbors(
     tree: KDTree,
     left_xyz: npt.NDArray[np.float64],
@@ -182,7 +183,7 @@ def _query_min_max_neighbors(
     """Finds `n_neighbors` within a distance range for all points in a pair of partitions"""
     left_tree = KDTree(left_xyz, compact_nodes=True, balanced_tree=True, copy_data=False)
 
-    # Find the number of neighbors within the minimum distance threshold
+    # Find the number of neighbors within the minimum distance threshold.
     len_too_close_neighbors = np.zeros(left_xyz.shape[0], dtype=np.int64)
     # The sparse distance matrix is a dictionary of keys. It contains the pairs of neighbors
     # and their respective distances, in the form of [i,j,distance], i and j being the indices
@@ -195,11 +196,11 @@ def _query_min_max_neighbors(
     unique, counts = np.unique(left_indices_too_close, return_counts=True)
     len_too_close_neighbors[unique] = counts
 
-    # Make sure we don't ask for more neighbors than there are points
+    # Make sure we don't ask for more neighbors than there are points.
     n_neighbors_to_request = min(n_neighbors + max(len_too_close_neighbors), len(right_xyz))
     distances, right_index = tree.query(left_xyz, k=n_neighbors_to_request, distance_upper_bound=max_distance)
 
-    # Create mask to filter neighbors that are too close. First we start with all false
+    # Create mask to filter neighbors that are too close. First we start with all false.
     mask = np.zeros((len(left_xyz), n_neighbors_to_request), dtype=bool)
 
     # Compute the indices of the mask that should be true e.g. for the mask [[0,0,1,1], [0,1,1,0], [1,1,0,0]]
@@ -208,21 +209,33 @@ def _query_min_max_neighbors(
 
     # The first array for the indices is [0 * n_neighbors, 1 * n_neighbors, ..., len(left) * n_neighbors]
     mask_ones_0 = np.repeat(np.arange(len_too_close_neighbors.shape[0]), n_neighbors)
-
     # The second array of the indices is [len_too_close[0], len_too_close[0] + 1, ... len_too_close[0]
     # + n_neighbors, repeated for each len_too_close]
     mask_ones_1 = np.tile(np.arange(n_neighbors), len_too_close_neighbors.shape[0]) + np.repeat(
         len_too_close_neighbors, n_neighbors
     )
-    # If there are fewer points than those requested, clip indices
+    # If there are fewer points than those requested, clip indices.
     if n_neighbors + max(len_too_close_neighbors) > len(right_xyz):
         mask_ones_1 = np.clip(mask_ones_1, None, len(right_xyz) - 1)
 
-    # Set the mask to one for the indices it should be one
+    # Set the mask to one for the indices it should be one.
     mask[mask_ones_0, mask_ones_1] = True
 
-    # Apply mask to filter points
-    distances = np.where(mask, distances, np.inf)
-    indices = np.where(mask, right_index, -1)
+    # We need to generate a final mask (out_mask) to remove all the leading False(s) from the first mask.
+    # These leading False(s) appear for points that have matches closer than the minimum distance radius.
+    out_mask = np.zeros((len(left_xyz), n_neighbors), dtype=bool)
+    out_mask_ones_1: np.ndarray = mask_ones_1 - np.repeat(len_too_close_neighbors, n_neighbors)
+    out_mask[mask_ones_0, out_mask_ones_1] = True
 
-    return distances, indices
+    # These arrays will hold the final distances and respective indices for the specified number
+    # of neighbors. By default, they are initialized with values for non-match, i.e., distances
+    # are filled with infinite values and indices with an index that corresponds to the number of
+    # points in the right partition.
+    out_distances = np.full((len(left_xyz), n_neighbors), fill_value=np.inf)
+    out_indexes = np.full((len(left_xyz), n_neighbors), fill_value=len(right_xyz), dtype=np.int64)
+
+    # Finally, apply mask to filter points.
+    out_indexes[out_mask] = right_index[mask]
+    out_distances[out_mask] = distances[mask]
+
+    return out_distances, out_indexes
