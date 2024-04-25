5855ed8f603e1c19b15b45969208b486ae943d50
Sean McGuire <seanmcgu@andrew.cmu.edu>
2024-04-25 16:15:44 -0400

    pylint

--- src/lsdb/__init__.py
+++ src/lsdb/__init__.py
@@ -8,6 +8,7 @@ if QUERY_PLANNING_ON is not False:
     warnings.warn("This version of lsdb does not support dataframe query-planning, which has been disabled.")
     dask.config.set({"dataframe.query-planning": False})
 
+# pylint: disable=wrong-import-position
 from ._version import __version__
 from .catalog import Catalog
 from .loaders import from_dataframe, read_hipscat
