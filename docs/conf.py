2672f5c1cf224272ef7e312c5d829428594444fd
Melissa DeLucchi <delucchi@andrew.cmu.edu>
2024-06-21 15:33:20 -0400

    Workflow diagram

--- docs/conf.py
+++ docs/conf.py
@@ -24,7 +24,13 @@ version = ".".join(release.split(".")[:2])
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ["sphinx.ext.mathjax", "sphinx.ext.napoleon", "sphinx.ext.viewcode", "sphinx.ext.intersphinx"]
+extensions = [
+    "sphinx.ext.mathjax",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.graphviz",
+]
 
 extensions.append("autoapi.extension")
 extensions.append("nbsphinx")
