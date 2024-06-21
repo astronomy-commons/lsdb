5d443faa12edc8ff73d675068ee80aaec2d199ad
Melissa DeLucchi <delucchi@andrew.cmu.edu>
2024-06-21 15:44:31 -0400

    Require svg,

--- docs/conf.py
+++ docs/conf.py
@@ -45,6 +45,8 @@ copybutton_prompt_text = ">> "
 ## lets us suppress the copy button on select code blocks.
 copybutton_selector = "div:not(.no-copybutton) > div.highlight > pre"
 
+graphviz_output_format = 'svg'
+
 templates_path = []
 exclude_patterns = ["_build", "**.ipynb_checkpoints"]
 
