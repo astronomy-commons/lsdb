b613df7210a212d3aa042399eb84856d00b61f1e
Sean McGuire <seanmcgu@andrew.cmu.edu>
2024-10-10 10:50:13 -0400

    isort

--- src/lsdb/catalog/dataset/healpix_dataset.py
+++ src/lsdb/catalog/dataset/healpix_dataset.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
-import dataclasses
 import copy
+import dataclasses
 import warnings
 from pathlib import Path
 from typing import Any, Callable, Dict, Iterable, List, Tuple
