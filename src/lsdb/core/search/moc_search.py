4816dfe5436d625d93aa28a5f11d47fc5240662b
Sean McGuire <seanmcgu@andrew.cmu.edu>
2024-11-18 16:50:07 -0500

    add moc_filter

---
+++ src/lsdb/core/search/moc_search.py
@@ -0,0 +1,35 @@
+from __future__ import annotations
+
+import astropy.units as u
+
+from typing import TYPE_CHECKING
+
+import nested_pandas as npd
+from hats.catalog import TableProperties
+from mocpy import MOC
+
+from lsdb.core.search.abstract_search import AbstractSearch
+
+if TYPE_CHECKING:
+    from lsdb.types import HCCatalogTypeVar
+
+
+class MOCSearch(AbstractSearch):
+    """Filter the catalog by HEALPix pixels.
+
+    Filters partitions in the catalog to those that are in a specified pixel set.
+    Does not filter points inside those partitions.
+    """
+
+    def __init__(self, moc: MOC, fine: bool = True):
+        super().__init__(fine)
+        self.moc = moc
+
+    def filter_hc_catalog(self, hc_structure: HCCatalogTypeVar) -> HCCatalogTypeVar:
+        return hc_structure.filter_by_moc(self.moc)
+
+    def search_points(self, frame: npd.NestedFrame, metadata: TableProperties) -> npd.NestedFrame:
+        df_ras = frame[metadata.ra_column].to_numpy()
+        df_decs = frame[metadata.dec_column].to_numpy()
+        mask = self.moc.contains_lonlat(df_ras * u.deg, df_decs * u.deg)
+        return frame.iloc[mask]
