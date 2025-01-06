965ef8f096b3840d29abe8ba3c4c4fd2c690c2af
Melissa DeLucchi <113376043+delucchi-cmu@users.noreply.github.com>
2025-01-06 13:08:36 -0500

    Use a naive sparse histogram. (#534)
    * Use a naive sparse histogram.
    
    * Remove make_from_counts

--- src/lsdb/io/to_hats.py
+++ src/lsdb/io/to_hats.py
@@ -6,13 +6,12 @@ from typing import TYPE_CHECKING
 
 import dask
 import hats as hc
-import hats.pixel_math.healpix_shim as hp
 import nested_pandas as npd
 import numpy as np
 from hats.catalog import PartitionInfo
 from hats.catalog.healpix_dataset.healpix_dataset import HealpixDataset as HCHealpixDataset
 from hats.pixel_math import HealpixPixel, spatial_index_to_healpix
-from hats.pixel_math.sparse_histogram import SparseHistogram
+from hats.pixel_math.sparse_histogram import HistogramAggregator, SparseHistogram
 from upath import UPath
 
 if TYPE_CHECKING:
@@ -42,7 +41,7 @@ def perform_write(
         at the specified order.
     """
     if len(df) == 0:
-        return 0, SparseHistogram.make_empty(histogram_order)
+        return 0, SparseHistogram([], [], histogram_order)
     pixel_dir = hc.io.pixel_directory(base_catalog_dir, hp_pixel.order, hp_pixel.pixel)
     hc.io.file_io.make_directory(pixel_dir, exist_ok=True)
     pixel_path = hc.io.paths.pixel_catalog_file(base_catalog_dir, hp_pixel)
@@ -64,7 +63,7 @@ def calculate_histogram(df: npd.NestedFrame, histogram_order: int) -> SparseHist
     order_pixels = spatial_index_to_healpix(df.index.to_numpy(), target_order=histogram_order)
     gb = df.groupby(order_pixels, sort=False).apply(len)
     indexes, counts_at_indexes = gb.index.to_numpy(), gb.to_numpy(na_value=0)
-    return SparseHistogram.make_from_counts(indexes, counts_at_indexes, histogram_order)
+    return SparseHistogram(indexes, counts_at_indexes, histogram_order)
 
 
 # pylint: disable=protected-access
@@ -116,11 +115,11 @@ def to_hats(
     )
     new_hc_structure.catalog_info.to_properties_file(base_catalog_path)
     # Save the point distribution map
-    full_histogram = np.zeros(hp.order2npix(histogram_order))
+    total_histogram = HistogramAggregator(histogram_order)
     for partition_hist in histograms:
-        full_histogram += partition_hist.to_array()
+        total_histogram.add(partition_hist)
     point_map_path = hc.io.paths.get_point_map_file_pointer(base_catalog_path)
-    hc.io.file_io.write_fits_image(full_histogram, point_map_path)
+    hc.io.file_io.write_fits_image(total_histogram.full_histogram, point_map_path)
 
 
 def write_partitions(
