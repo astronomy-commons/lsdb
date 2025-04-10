7768fa6fae1d60efff02a62862b0a7fd7217ea1f
Melissa DeLucchi <delucchi@andrew.cmu.edu>
2025-04-10 12:09:17 -0400

    Expand possible pixelsearch inputs

--- tests/lsdb/catalog/test_pixel_search.py
+++ tests/lsdb/catalog/test_pixel_search.py
@@ -1,5 +1,6 @@
 import nested_dask as nd
 import pandas as pd
+import pytest
 from hats.pixel_math import HealpixPixel
 
 from lsdb.core.search.pixel_search import PixelSearch
@@ -20,6 +21,67 @@ def test_pixel_search(small_sky_catalog, small_sky_order1_catalog):
         HealpixPixel(1, 46),
         HealpixPixel(1, 47),
     ] == catalog.get_healpix_pixels()
+
+
+def test_pixel_search_from_radec(small_sky_catalog, small_sky_order1_catalog):
+    """Searching for pixels, based on radec values"""
+    ## First, try with just one radec value
+    search_object = PixelSearch.from_radec(282.5, -58.5)
+    assert len(search_object.pixels) == 1
+
+    search_result = small_sky_catalog.search(search_object)
+    assert [HealpixPixel(0, 11)] == search_result.get_healpix_pixels()
+
+    search_result = small_sky_order1_catalog.search(search_object)
+    assert [HealpixPixel(1, 46)] == search_result.get_healpix_pixels()
+
+    ## Two radec values in the same catalog pixel
+    search_object = PixelSearch.from_radec([282.5, 283.5], [-58.5, -61.5])
+    assert len(search_object.pixels) == 2
+
+    search_result = small_sky_catalog.search(search_object)
+    assert [HealpixPixel(0, 11)] == search_result.get_healpix_pixels()
+
+    search_result = small_sky_order1_catalog.search(search_object)
+    assert [HealpixPixel(1, 46)] == search_result.get_healpix_pixels()
+
+    ## radec values from every order 1 pixel in the catalog
+    search_object = PixelSearch.from_radec(
+        [282.5, 283.5, 308.5, 346.5, 319.5], [-58.5, -61.5, -69.5, -60.5, -35.5]
+    )
+    assert len(search_object.pixels) == 5
+
+    search_result = small_sky_catalog.search(search_object)
+    assert [HealpixPixel(0, 11)] == search_result.get_healpix_pixels()
+
+    search_result = small_sky_order1_catalog.search(search_object)
+    assert [
+        HealpixPixel(1, 44),
+        HealpixPixel(1, 45),
+        HealpixPixel(1, 46),
+        HealpixPixel(1, 47),
+    ] == search_result.get_healpix_pixels()
+
+
+def test_pixel_search_types():
+    """Check that we can initialize PixelSearch with a variety of types."""
+    search_object = PixelSearch([(1, 44)])
+    assert search_object.pixels == [HealpixPixel(1, 44)]
+
+    search_object = PixelSearch((1, 44))
+    assert search_object.pixels == [HealpixPixel(1, 44)]
+
+    search_object = PixelSearch([HealpixPixel(1, 44)])
+    assert search_object.pixels == [HealpixPixel(1, 44)]
+
+    search_object = PixelSearch(HealpixPixel(1, 44))
+    assert search_object.pixels == [HealpixPixel(1, 44)]
+
+    with pytest.raises(ValueError, match="pixels required"):
+        PixelSearch([])
+
+    with pytest.raises(ValueError, match="Unsupported input"):
+        PixelSearch(PixelSearch((1, 44)))
 
 
 def test_pixel_search_is_empty(small_sky_catalog, small_sky_order1_catalog):
