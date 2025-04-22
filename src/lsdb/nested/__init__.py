625908d0adac370132ac96d339d8d297d2e639b4
Doug Branton <brantd@uw.edu>
2025-04-22 10:55:16 -0700

    remove version from init

--- src/lsdb/nested/__init__.py
+++ src/lsdb/nested/__init__.py
@@ -3,4 +3,3 @@ from .core import NestedFrame  # noqa
 from .io import read_parquet  # noqa
 from .datasets import generate_data  # noqa
 from .utils import count_nested  # noqa
-from ._version import __version__  # noqa
