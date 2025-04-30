616003ff58e6cf3ffcf2642f1588471c7d0b8eaa
Doug Branton <brantd@uw.edu>
2025-04-30 13:32:13 -0700

    WIP: generate_data with ra/dec; generate_catalog

--- src/lsdb/nested/__init__.py
+++ src/lsdb/nested/__init__.py
@@ -1,5 +1,5 @@
 from . import accessor, backends  # noqa
 from .core import NestedFrame  # noqa
-from .datasets import generate_data  # noqa
+from .datasets import generate_data, generate_catalog  # noqa
 from .io import read_parquet  # noqa
 from .utils import count_nested  # noqa
--- src/lsdb/nested/datasets/generation.py
+++ src/lsdb/nested/datasets/generation.py
@@ -1,9 +1,12 @@
 from nested_pandas import datasets
+import numpy as np
 
 from lsdb.nested.core import NestedFrame
+import lsdb
 
 
-def generate_data(n_base, n_layer, npartitions=1, seed=None) -> NestedFrame:
+def generate_data(n_base, n_layer, npartitions=1, 
+                  seed=None, ra_range=(0.0,360.0), dec_range=(-90,90)) -> NestedFrame:
     """Generates a toy dataset.
 
     Docstring copied from nested-pandas.
@@ -20,6 +23,10 @@ def generate_data(n_base, n_layer, npartitions=1, seed=None) -> NestedFrame:
         The number of partitions to split the data into.
     seed : int
         A seed to use for random generation of data
+    ra_range : tuple
+        A tuple of the min and max values for the ra column in degrees
+    dec_range : tuple
+        A tuple of the min and max values for the dec column in degrees
 
     Returns
     -------
@@ -28,22 +35,29 @@ def generate_data(n_base, n_layer, npartitions=1, seed=None) -> NestedFrame:
 
     Examples
     --------
-    >>> import lsdb.nested as nd # doctest: +SKIP
-    >>> nd.datasets.generate_data(10,100) # doctest: +SKIP
-    >>> nd.datasets.generate_data(10, {"nested_a": 100, "nested_b": 200}) # doctest: +SKIP
+    >>> from lsdb.nested.datasets import generate_data # doctest: +SKIP
+    >>> generate_data(10,100) # doctest: +SKIP
+    >>> generate_data(10, {"nested_a": 100, "nested_b": 200}) # doctest: +SKIP
     """
 
     # Use nested-pandas generator
     base_nf = datasets.generate_data(n_base, n_layer, seed=seed)
 
-    # Convert to nested-dask
+    # Generated "ra" and "dec" columns for hats catalog validity
+    rng = np.random.default_rng(seed)  # Use the provided seed for reproducibility
+    base_nf["ra"] = rng.uniform(ra_range[0], ra_range[1], size=n_base)
+    base_nf["dec"] = rng.uniform(dec_range[0], dec_range[1], size=n_base)
+
+    # Convert to lsdb.nested NestedFrame
     base_nf = NestedFrame.from_pandas(base_nf).repartition(npartitions=npartitions)
 
     return base_nf
 
+def generate_catalog(n_base, n_layer,
+                     seed=None, ra_range=(0.0,360.0), dec_range=(-90,90), **kwargs):
+    """Generates a toy catalog.
 
-def generate_parquet_file(n_base, n_layer, path, file_per_layer=True, npartitions=1, seed=None):
-    """Generates a toy dataset and outputs it to one or more parquet files.
+    Docstring copied from nested-pandas.
 
     Parameters
     ----------
@@ -53,22 +67,27 @@ def generate_parquet_file(n_base, n_layer, path, file_per_layer=True, npartition
         The number of rows per n_base row to generate for a nested layer.
         Alternatively, a dictionary of layer label, layer_size pairs may be
         specified to created multiple nested columns with custom sizing.
-    path : str,
-        The path to the parquet file to write to if `file_per_layer` is `False`,
-        and otherwise the path to the directory to write the parquet file for
-        each layer.
-    file_per_layer : bool, default=True
-        TODO: Currently only True is supported.
-        If True, write each layer to its own parquet file. Otherwise, write
-        the generated to a single parquet file representing a nested dataset.
-    npartitions : int, default=1
-        The number of Dask partitions to split the generated data into for each layer.
-    seed : int, default=None
+    seed : int
         A seed to use for random generation of data
+    ra_range : tuple
+        A tuple of the min and max values for the ra column in degrees
+    dec_range : tuple
+        A tuple of the min and max values for the dec column in degrees
+    **kwargs :
+        Additional keyword arguments to pass to `lsdb.from_dataframe`.
 
     Returns
     -------
-    None
+    Catalog
+        The constructed LSDB Catalog.
+
+    Examples
+    --------
+    >>> from lsdb.nested.datasets import generate_catalog # doctest: +SKIP
+    >>> generate_catalog(10,100) # doctest: +SKIP
+    >>> generate_catalog(1000, 10, ra_range=(0.,10.), dec_range=(-5.,0.)) # doctest: +SKIP
     """
-    nf = generate_data(n_base, n_layer, npartitions, seed)
-    nf.to_parquet(path, by_layer=file_per_layer, write_index=False)
+
+    base_nf = generate_data(n_base, n_layer, seed=seed, ra_range=ra_range, dec_range=dec_range)
+    return lsdb.from_dataframe(base_nf.compute(), **kwargs)
+
