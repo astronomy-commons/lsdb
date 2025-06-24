6cbb6d42c0584fd6a569de1a5740a87579865421
Sean McGuire <seanmcgu@andrew.cmu.edu>
2025-06-24 18:51:55 -0400

    wip

---
+++ src/lsdb/core/source_association/abstract_object_aggregator.py
@@ -0,0 +1,34 @@
+from abc import ABC, abstractmethod
+from typing import TYPE_CHECKING
+
+import nested_pandas as npd
+import numpy as np
+from hats import HealpixPixel
+from hats.catalog import TableProperties
+
+if TYPE_CHECKING:
+    from lsdb.catalog import Catalog
+
+
+class AbstractObjectAggregator(ABC):
+
+    def validate(self, catalog: Catalog):
+        pass
+
+    def aggregate_objects(
+        self,
+        partition: npd.NestedFrame,
+        pixel: HealpixPixel,
+        properties: TableProperties,
+        margin_properties: TableProperties,
+        object_ids: np.ndarray,
+    ) -> npd.NestedFrame:
+        pass
+
+    @abstractmethod
+    def perform_object_aggregation(self) -> npd.NestedFrame:
+        pass
+
+    @abstractmethod
+    def get_meta_df(self) -> npd.NestedFrame:
+        pass
