63c0242406db1257a096ba292766aea6c07b4adc
clytieq <113843835+clytieq@users.noreply.github.com>
2025-07-21 13:28:20 -0400

    assert unique expids

--- src/lsdb/core/source_association/baseline_source_associator.py
+++ src/lsdb/core/source_association/baseline_source_associator.py
@@ -34,46 +34,47 @@ class BaselineSourceAssociationAlgorithm(AbstractSourceAssociationAlgorithm):
         s_ids = df[source_id_col].to_numpy()
         # Get max xmatch distance as a chord
         max_dist_chord = _get_chord_distance(self.max_distance_arcsec)
-        # Make array to assign object ids
+        # Make array to assign object ids to sources
         object_id_assignment = np.full(s_ids.shape, fill_value=-1)
         # Make array of the objects we've found so far as the indices of the source df they correspond to
         first_object_inds = np.array([])
 
         for e_id in unique_exposures:
             # Find rows in this exposure
-            mask = exposures == e_id
+            e_mask = exposures == e_id
             if np.all(object_id_assignment == -1):
-                # If first exposure, assign all as new objects
-                object_id_assignment[mask] = s_ids[mask]
-                object_inds = np.where(mask)[0]
+                # If first exposure, assign all as new objects (obj id is source id)
+                object_id_assignment[e_mask] = s_ids[e_mask]
+                object_inds = np.where(e_mask)[0]
                 first_object_inds = object_inds
                 continue
             # Get the coordinates of the objects we've found so far
-            object_xyz = xyz[first_object_inds]
+            objects_xyz = xyz[first_object_inds]
             # Get the coordinates of the current exposure
-            source_xyz = xyz[mask]
-            # Crossmatch the current exposure to the current objects
-            distances, source_idx, object_idx = _find_crossmatch_indices(
-                source_xyz, object_xyz, n_neighbors=1, max_distance=max_dist_chord
+            sources_xyz = xyz[e_mask]
+            # Crossmatch the current exposure to the current objects: get indices into _xyz arrays
+            xmatch_distances, xmatch_sources_idx, xmatch_objects_idx = _find_crossmatch_indices(
+                sources_xyz, objects_xyz, n_neighbors=1, max_distance=max_dist_chord
             )
             # Ensure each object matches to at most one source in this exposure
-            unique_object_idx, object_idx_counts = np.unique(object_idx, return_counts=True)
+            unique_object_idx, object_idx_counts = np.unique(xmatch_objects_idx, return_counts=True)
             for object_id in unique_object_idx[object_idx_counts > 1]:
                 # Index into crossmatch indices
-                idx_idx = np.where(object_idx == object_id)[0]
-                extra_idx_idx = np.argsort(idx_idx)[1:]
-                # Remove any matches that aren't the closest match
-                distances = np.delete(distances, idx_idx)
-                source_idx = np.delete(source_idx, extra_idx_idx)
-                object_idx = np.delete(object_idx, extra_idx_idx)
-            if len(object_idx) > 0:
+                xmatch_idx_idx = np.where(xmatch_objects_idx == object_id)[0]
+                xmatch_idx_idx_dups_idx = np.argsort(xmatch_distances[xmatch_idx_idx])[1:]
+                xmatch_dups_idx_idx = xmatch_idx_idx[xmatch_idx_idx_dups_idx]
+                # Remove (by index) any matches that aren't the closest match
+                xmatch_distances = np.delete(xmatch_distances, xmatch_dups_idx_idx)
+                xmatch_sources_idx = np.delete(xmatch_sources_idx, xmatch_dups_idx_idx)
+                xmatch_objects_idx = np.delete(xmatch_objects_idx, xmatch_dups_idx_idx)
+            if len(xmatch_objects_idx) > 0:
                 # Assign the Object ID of the objects to their matched sources
-                matched_source_inds = np.where(mask)[0][source_idx]
-                object_inds_for_matches = object_id_assignment[first_object_inds[object_idx]]
+                matched_source_inds = np.where(e_mask)[0][xmatch_sources_idx]
+                object_inds_for_matches = object_id_assignment[first_object_inds[xmatch_objects_idx]]
                 object_id_assignment[matched_source_inds] = object_inds_for_matches
             # Assign new IDs to the unmatched sources, and add them as new objects
-            remaining_source_idx = np.delete(np.arange(len(source_xyz)), source_idx)
-            remaining_source_inds = np.where(mask)[0][remaining_source_idx]
+            remaining_source_idx = np.delete(np.arange(len(sources_xyz)), xmatch_sources_idx)
+            remaining_source_inds = np.where(e_mask)[0][remaining_source_idx]
             object_id_assignment[remaining_source_inds] = s_ids[remaining_source_inds]
             first_object_inds = np.concat([first_object_inds, remaining_source_inds])
         # Assign the object id assigment and return the Pandas Series to get the right index
