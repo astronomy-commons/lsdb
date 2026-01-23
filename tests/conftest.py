f7baec1e98127f3bb659ee1ba8106e467a7911a7
Melissa DeLucchi <delucchi@andrew.cmu.edu>
2026-01-23 15:54:11 -0500

    Appease mypy

--- tests/conftest.py
+++ tests/conftest.py
@@ -471,9 +471,9 @@ class Helpers:
         if do_not_compare is None:
             do_not_compare = []
         do_not_compare.extend(["hats_creation_date", "hats_estsize"])
-        do_not_compare = {prop: None for prop in do_not_compare}
-        expected_catalog_info = expected_catalog_info.copy_and_update(**do_not_compare)
-        catalog_info = catalog_info.copy_and_update(**(properties_to_update | do_not_compare))
+        do_not_compare_dict = {prop: None for prop in do_not_compare}
+        expected_catalog_info = expected_catalog_info.copy_and_update(**do_not_compare_dict)
+        catalog_info = catalog_info.copy_and_update(**(properties_to_update | do_not_compare_dict))
         assert expected_catalog_info.explicit_dict() == catalog_info.explicit_dict()
         if check_extra_properties:
             assert expected_catalog_info.extra_dict() == catalog_info.extra_dict()
