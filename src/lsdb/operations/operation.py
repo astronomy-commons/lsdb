0640c33916917bf2e2946ff3f10be24140126068
Doug Branton <dougrbranton@gmail.com>
2026-06-12 14:17:27 -0700

    docstrings and no coverage for ABC

--- src/lsdb/operations/operation.py
+++ src/lsdb/operations/operation.py
@@ -15,53 +15,59 @@ class HealpixGraph:
 
     @property
     def keys(self):
+        """Returns the list of keys in the graph."""
         return list(self.pixel_to_key_map.values())
 
 
 class Operation(ABC):
+    """Abstract base class defining an operation."""
+
     allow_column_projection_passthrough = False
 
     @property
     @abstractmethod
-    def name(self) -> str:
+    def name(self) -> str:  # pragma: no cover
         """The name of the Task"""
         pass
 
     @property
     @abstractmethod
-    def key_name(self) -> str:
+    def key_name(self) -> str:  # pragma: no cover
+        """The name of the key in the task graph that corresponds to this operation's output"""
         pass
 
     @abstractmethod
-    def build(self) -> HealpixGraph:
+    def build(self) -> HealpixGraph:  # pragma: no cover
         """Returns the task graph for this operation, where each node corresponds to a HEALPix pixel"""
         pass
 
     @property
     @abstractmethod
-    def meta(self) -> npd.NestedFrame:
+    def meta(self) -> npd.NestedFrame:  # pragma: no cover
         """Returns the metadata for the output of this operation"""
         pass
 
     @property
     @abstractmethod
-    def dependencies(self) -> list[Operation]:
+    def dependencies(self) -> list[Operation]:  # pragma: no cover
         """Returns the list of input operations for this operation"""
         pass
 
     @property
-    def is_reloadable(self) -> bool:
+    def is_reloadable(self) -> bool:  # pragma: no cover
         """Whether this operation can be reconstructed from disk via _reload_with_filter."""
         return False
 
     @property
     @abstractmethod
-    def healpix_pixels(self) -> list[HealpixGraph]:
+    def healpix_pixels(self) -> list[HealpixGraph]:  # pragma: no cover
         """Returns the list of HEALPix pixels that this operation's partitions correspond to"""
         pass
 
-    def optimize(self) -> Self:
+    def optimize(self) -> Self:  # pragma: no cover
+        """Returns an optimized version of the operation."""
         return self
 
-    def __repr__(self):
+    def __repr__(self):  # pragma: no cover
+        """String representation of the operation."""
         return self.name
